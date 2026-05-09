28e1f598d1b993009a5602fd1432a65c8a8dc928
Carlos Andres Muñoz Leyton <carlo.munozl@duocuc.cl>
2026-05-09 05:19:08 -0400

    Fix CORS agrega URL de Railway en los 3 servicios

--- reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/Config/WebConfig.java
+++ reportes-service/src/main/java/petly/sanosysalvos/cl/reportes/Config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
      public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/petly/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                    "http://localhost:5173",
+                    "https://petly-docker-frontend-production.up.railway.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
--- usuarios-service/src/main/java/petly/sanosysalvos/cl/usuarios/Config/CorsConfig.java
+++ usuarios-service/src/main/java/petly/sanosysalvos/cl/usuarios/Config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/petly/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://petly-docker-frontend-production.up.railway.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
